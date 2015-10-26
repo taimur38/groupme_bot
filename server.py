c4d416d126c5388b63ca12939fc6568c0d2797e2
Taimur Shah <taimur38@gmail.com>
2015-10-25 20:47:31 -0500

    links endpoint

--- server.py
+++ server.py
@@ -3,6 +3,8 @@ import logging
 
 from flask import Flask, request
 
+import constants
+from py2neo import Graph
 from listeners import listeners
 from helpers import post_message
 
@@ -11,6 +13,7 @@ urls = []
 
 logging.basicConfig(filename="debug.log", level=logging.INFO)
 
+graph = Graph(constants.neo_url)
 
 @app.route('/groupme', methods=['POST'])
 def message():
@@ -34,6 +37,34 @@ def message():
 def say(msg):
     post_message(msg)
     return 'sent ' + msg
+
+
+@app.route('/links')
+def get_links():
+    link_statement = """
+    MATCH (u:User)--(m:Message)--(l:Link)
+    return u.name as name, m.date as date, l.id as link
+    order by toint(date) desc
+    limit 15
+    """
+
+    rsp = graph.cypher.execute(link_statement)
+
+    template = """
+    <div><a href="{link}">{user} submitted on {date}</a></div>
+    """
+
+    divs = ""
+    for r in rsp:
+		datestring = datetime.fromtimestamp(int(r['date'])).strftime('%m/%d %H:%M:%S')
+        divs += template.format(link=r['link'], user=r['name'], date=datestring)
+
+    return """
+    <html>
+    <body>
+    {divs}
+    </body>
+    </html>""".format(divs=divs)
 
 
 @app.route("/")
