eec17938e41ae39f326a42ece71a625b465f7674
Taimur <taimur38@gmail.com>
2016-03-14 01:26:24 -0400

    rotating tasks

--- listeners/task.py
+++ listeners/task.py
@@ -1,17 +1,18 @@
-import random
-
 import constants
 from helpers import post_message
+
+index = 0
 
 
 def onMessage(message):
     if 'juanita' not in message['text'].lower():
         return
 
-    person = random.choice(constants.people)
-    adj = random.choice(constants.adjectives)
+    global index
+    index = (index + 1) % len(constants.people)
+    person = constants.people[index]
 
     if person == 'Taimur':
         post_message("Master Shah, I'm afraid chance has called upon thee to perform this duty")
     else:
-        post_message('make that {adj} {person} do it'.format(adj=adj, person=person))
+        post_message('make {person} do it'.format(person=person))
